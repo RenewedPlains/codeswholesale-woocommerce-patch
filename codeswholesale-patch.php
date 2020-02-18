02f248b755e1b790e6e41921954c02b8f1012eb7
Mario Freuler <renewedplains@gmail.com>
2020-02-19 00:05:03 +0100

    added changes for apply to the official store
    wordpress.org

--- codeswholesale-patch.php
+++ codeswholesale-patch.php
@@ -1,11 +1,13 @@
 <?php
 /*
-Plugin Name: Bojett Codeswholesale Patch
-Description: Currently the codeswholesale importer for WooCommerce is not working. This patch provides a new importer to import all products from CWS.
+Plugin Name: Bojett.com Codeswholesale Plus
+Plugin URI: https://github.com/RenewedPlains/codeswholesale-woocommerce-patch
+Description: Bojett.com Codeswholesale Plus is installed as an additional plugin, which retrieves the access data from the Codeswholesale for WooCommerce Plugin API and starts a new import process via the V2 API of Codeswholesale.
 Text Domain: codeswholesale_patch
 Depends: WooCommerce
 Version: 0.8.4
-Author: Mario Freuler, Bojett.com
+Author: Mario Freuler
+Author URI: https://www.bojett.com
 License: GPL2
 */
 ob_start();
@@ -248,10 +250,6 @@ add_action( 'check_update_bearer_token', 'run_cws_cron_script' );
 check_update_bearer_token();
 
 
-
-
-//import_batch();
-
 function bojett_settings() {
     global $table_prefix, $wpdb;
     if($_GET['notvalid'] == 'true') {
@@ -412,7 +410,6 @@ function bojett_settings() {
     <?php
 }
 
-
 function get_string_between($string, $start, $end){
     $string = ' ' . $string;
     $ini = strpos($string, $start);
@@ -420,36 +417,8 @@ function get_string_between($string, $start, $end){
     $ini += strlen($start);
     $len = strpos($string, $end, $ini) - $ini;
     return substr($string, $ini, $len);
-}/*
-// Get the bearer authorizationcode from your database and recheck the expiringtime
-$table_name = $wpdb->prefix . "codeswholesale_access_tokens";
-$access_bearer = $wpdb->get_results( "SELECT expires_in, access_token FROM $table_name" );
-$db_token = $access_bearer[0]->access_token;
-$db_expires_in = $access_bearer[0]->expires_in;
-$current_timestamp = time();
+}
 
-if($db_expires_in > $current_timestamp) {
-    // Do nothing, the bearer is already up to date.
-} else {
-    $options_name = $wpdb->prefix . "options";
-    $cws_credentials = $wpdb->get_results( "SELECT option_value FROM $options_name WHERE option_name = 'cw_options'" );
-    $client_id = get_string_between($cws_credentials[0]->option_value, 'api_client_id";s:32:"', '"');
-    $client_secret = get_string_between($cws_credentials[0]->option_value, 's:17:"api_client_secret";s:60:"', '"');
-    $client_signature = get_string_between($cws_credentials[0]->option_value, 's:20:"api_client_singature";s:36:"', '"');
-
-    $ch = curl_init('https://api.codeswholesale.com/oauth/token?grant_type=client_credentials&client_id=' . $client_id . '&client_secret=' . $client_secret); // Initialise cURL
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json' )); // Inject the token into the header
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1); // This will follow any redirects
-    $result = curl_exec($ch); // Execute the cURL statement
-    $new_bearer = json_decode($result, true)['access_token'];
-    $new_bearer_expires = json_decode($result, true)['expires_in'];
-    $new_db_expires_in = $current_timestamp + $new_bearer_expires;
-    $table_name = $wpdb->prefix . "codeswholesale_access_tokens";
-
-    $wpdb->update($table_name, array('expires_in' => $new_db_expires_in, 'access_token' => $new_bearer), array('scope' => 'administration'), array('%s', '%s'), array('%s'));
-    curl_close($ch); // Close the cURL connection
-}*/
 function isa_add_cron_recurrence_interval( $schedules ) {
 
     $schedules['bojett_cws_import'] = array(
