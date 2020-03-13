27973ac5459a641fafff2304338bd3bcc9a978cc
Mario Freuler <renewedplains@gmail.com>
2020-03-13 18:41:56 +0100

    added guzzle support for replacing cURL

---
+++ vendor/guzzlehttp/guzzle/src/Exception/ConnectException.php
@@ -0,0 +1,33 @@
+<?php
+namespace GuzzleHttp\Exception;
+
+use Psr\Http\Client\NetworkExceptionInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * Exception thrown when a connection cannot be established.
+ *
+ * Note that no response is present for a ConnectException
+ */
+class ConnectException extends RequestException implements NetworkExceptionInterface
+{
+    public function __construct(
+        string $message,
+        RequestInterface $request,
+        \Throwable $previous = null,
+        array $handlerContext = []
+    ) {
+        parent::__construct($message, $request, null, $previous, $handlerContext);
+    }
+
+    public function getResponse(): ?ResponseInterface
+    {
+        return null;
+    }
+
+    public function hasResponse(): bool
+    {
+        return false;
+    }
+}
